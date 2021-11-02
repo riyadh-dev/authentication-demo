8fa816d71218973c515c038196d4af8473af1eae
riyadh-dev <baatchia_riyadh@protonmail.com>
2021-11-02 15:35:32 +0100

    feat(client): add sign up

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,25 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
-import './App.css';
-import logo from './logo.svg';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import SignUp from './components/SignUp';
 
 function App() {
+	const queryClient = new QueryClient();
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-          Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn React
-				</a>
-			</header>
-		</div>
+		<ChakraProvider>
+			<QueryClientProvider client={queryClient} >
+				<SignUp/>
+			</QueryClientProvider>
+		</ChakraProvider>
 	);
 }
 
