ff60a8d68b494e078aa9bba520e7b523e7fd7428
riyadh-dev <Baatchia_Riyadh@protonmail.com>
2021-11-03 22:46:41 +0100

    feat(server): add login

--- packages/server/src/USER/controllers.ts
+++ packages/server/src/USER/controllers.ts
@@ -1,6 +1,9 @@
+import bcrypt from 'bcryptjs';
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { IErrorHandler, isErrorWithCode } from '../common/interfaces';
 import { catchAsyncMiddlewareError } from '../common/middlewares';
+import { IUser } from './interfaces';
 import { userModel } from './model';
 
 const createUserErrorHandler: IErrorHandler = (error, res) => {
@@ -27,3 +30,34 @@ const createUserUnsafe = async (req: Request, res: Response) => {
 };
 
 export const createUser = catchAsyncMiddlewareError(createUserUnsafe, createUserErrorHandler);
+
+const loginUnsafe = async (req: Request, res: Response) => {
+	const bodyUser: IUser = res.locals.validatedBody;
+	const userDoc = await userModel.findOne({ username: bodyUser.username });
+	if (!userDoc) {
+		res.status(400).json({ error: 'wrong password or username' });
+		return;
+	}
+
+	const { password, ...payload } = userDoc.toObject({ versionKey: false });
+	const isPasswordCorrect = bcrypt.compareSync(bodyUser.password, password);
+	if (!isPasswordCorrect) {
+		res.status(400).json({ error: 'wrong password or username' });
+		return;
+	}
+
+	const jwtToken = jwt.sign(payload, 'secret');
+	res.cookie('token', jwtToken, {
+		httpOnly: true,
+		signed: true,
+		secure: true,
+	});
+
+	const csrfToken = req.csrfToken();
+	res.status(200).json({
+		...payload,
+		csrfToken,
+	});
+};
+
+export const login = catchAsyncMiddlewareError(loginUnsafe);
--- packages/server/src/USER/router.ts
+++ packages/server/src/USER/router.ts
@@ -1,10 +1,21 @@
+import csurf from 'csurf';
 import { Router } from 'express';
 import { validateInput } from '../common/middlewares';
-import { createUser } from './controllers';
+import { createUser, login } from './controllers';
 import { validationSchema } from './validation';
 
 const router = Router();
 
+const LoginCsurf = csurf({
+	ignoreMethods: ['POST'],
+	cookie: {
+		httpOnly: true,
+		signed: true,
+		secure: true,
+	},
+});
+
 router.post('/create', validateInput(validationSchema), createUser);
+router.post('/login', LoginCsurf, validateInput(validationSchema), login);
 
 export default router;
