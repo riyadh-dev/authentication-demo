2f0741f93ab5b830d6bf6b4af7f894d4602bec39
riyadh-dev <baatchia_riyadh@protonmail.com>
2021-11-07 14:04:24 +0100

    build(heroku): adjust env vars and secrets for heroku

--- packages/server/src/util/secrets.ts
+++ packages/server/src/util/secrets.ts
@@ -1,20 +1,22 @@
 import dotenv from 'dotenv';
 import fs from 'fs';
 
+export const ENVIRONMENT = process.env.NODE_ENV ?? '';
+export const isProd = ENVIRONMENT === 'production';
+
 if (fs.existsSync('.env')) {
 	console.log('Using .env file to supply config environment variables');
 	dotenv.config({ path: '.env' });
+} else if (isProd) {
+	console.log('Using process.env to supply config environment variables');
 } else {
 	console.log('Using .env.example file to supply config environment variables');
 	dotenv.config({ path: '.env.example' });
 }
 
-export const ENVIRONMENT = process.env.NODE_ENV ?? '';
-export const isProd = ENVIRONMENT === 'production';
-
 export const JWT_SECRET = process.env['JWT_SECRET'] ?? '';
 export const MONGODB_URI = process.env['MONGODB_URI'] ?? '';
-export const SERVER_PORT = process.env['SERVER_PORT'] ?? '';
+export const SERVER_PORT = process.env['PORT'] ?? '';
 export const COOKIE_SECRET = process.env['COOKIE_SECRET'] ?? '';
 export const CLIENT_ORIGIN = process.env['CLIENT_ORIGIN'] ?? '';
 
