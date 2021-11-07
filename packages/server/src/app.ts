7e14eea64ca4b99e8a7472b7ecfde59df700c86c
riyadh-dev <baatchia_riyadh@protonmail.com>
2021-11-07 13:43:15 +0100

    feat(server): serve client build from server

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
+import path from 'path';
 import { handlePassedError } from './common/middlewares';
 import userRouter from './USER/router';
 import { CLIENT_ORIGIN, COOKIE_SECRET } from './util/secrets';
@@ -10,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser(COOKIE_SECRET));
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(
 	cors({
 		origin: CLIENT_ORIGIN,
@@ -19,6 +20,11 @@ app.use(
 );
 
 app.use('/api/user', userRouter);
+
+app.use(express.static(path.join(__dirname, '../../client/build')));
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+});
 
 app.use(handlePassedError);
 
